ebafa25818ee291eea93f84db8b061910beb3dfa
unknown <jaimuepe@gmail.com>
2016-04-29 21:25:21 +0200

    Example 000 - Extending Player Controller

---
+++ EX000_Extending_PlayerController/ExtPlayerController/Source/ExtPlayerController/ExtPlayerControllerGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ExtPlayerControllerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXTPLAYERCONTROLLER_API AExtPlayerControllerGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ script_symlink_starter_content.bat
@@ -0,0 +1,21 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+       net session >nul 2>&1
+    if %errorLevel% == 0 (
+        goto make_sym_link
+    ) else (
+        echo Se necesita ejecutar este script en modo administrador.
+		goto end
+    )
+	
+:make_sym_link
+set /p project_main_folder="Ruta de la carpeta principal del proyecto: "
+set current_directory = %~dp0
+
+mklink /d "%project_main_folder%\Content\StarterContent" "%current_directory%\StarterContent"
+
+
+:end
+pause
