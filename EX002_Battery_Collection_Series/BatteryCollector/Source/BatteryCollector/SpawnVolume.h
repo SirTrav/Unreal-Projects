9ab557d64ca16c937008ac02a825ebbe4dc1ba7f
unknown <jaimuepe@gmail.com>
2016-04-30 00:59:02 +0200

    Example 002 - Battery Collection (VIDEO 6)

---
+++ EX002_Battery_Collection_Series/BatteryCollector/Source/BatteryCollector/SpawnVolume.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	FORCEINLINE UBoxComponent* GetWhereToSpawn() const { return WhereToSpawn; }
+
+	/** Encuentra un punto aleatorio en el BoxComponent */
+	UFUNCTION(BlueprintPure, Category = "Spawning")
+	FVector GetRandomPointInVolume();
+
+private:
+	/** BoxComponent que indica dónde podrán spawnear baterías*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning", meta = (AllowPrivateAccess = true))
+	UBoxComponent* WhereToSpawn;
+};
