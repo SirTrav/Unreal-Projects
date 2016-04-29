db7b00cc69a5463e5a78476ee9d163b98bf6018e
unknown <jaimuepe@gmail.com>
2016-04-29 22:17:33 +0200

    Example 001 - Pointers and references

---
+++ EX001_Pointers_References/PointersAndRefs/Source/PointersAndRefs/PointersAndRefsGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PointersAndRefs.h"
+#include "PointersAndRefsGameMode.h"
+
+
+
+
---
+++ EX001_Pointers_References/PointersAndRefs/Source/PointersAndRefs/PointersAndRefsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PointersAndRefsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POINTERSANDREFS_API APointersAndRefsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
