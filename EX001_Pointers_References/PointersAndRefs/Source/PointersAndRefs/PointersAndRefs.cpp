f3c538d39f46d1f108f85fd1f8b81bd460cf357e
unknown <jaimuepe@gmail.com>
2016-04-29 22:27:08 +0200

    Revert "Example 001 - Pointers and references"
    This reverts commit db7b00cc69a5463e5a78476ee9d163b98bf6018e.

--- EX001_Pointers_References/PointersAndRefs/Source/PointersAndRefs/PointersAndRefs.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PointersAndRefs.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PointersAndRefs, "PointersAndRefs" );
--- EX001_Pointers_References/PointersAndRefs/Source/PointersAndRefs/PointersAndRefsGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PointersAndRefs.h"
-#include "PointersAndRefsGameMode.h"
-
-
-
-
