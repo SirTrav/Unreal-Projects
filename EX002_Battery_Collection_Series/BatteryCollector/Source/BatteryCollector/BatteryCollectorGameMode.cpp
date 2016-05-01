f93afedb4b3d4946de1a8038e561277c067fcb62
unknown <jaimuepe@gmail.com>
2016-05-01 18:38:33 +0200

    Example 002 - Battery Collection - Powering Down the Character

--- EX002_Battery_Collection_Series/BatteryCollector/Source/BatteryCollector/BatteryCollectorGameMode.cpp
+++ EX002_Battery_Collection_Series/BatteryCollector/Source/BatteryCollector/BatteryCollectorGameMode.cpp
@@ -3,6 +3,7 @@
 #include "BatteryCollector.h"
 #include "BatteryCollectorGameMode.h"
 #include "BatteryCollectorCharacter.h"
+#include "Kismet/GameplayStatics.h"
 
 ABatteryCollectorGameMode::ABatteryCollectorGameMode()
 {
@@ -11,5 +12,18 @@ ABatteryCollectorGameMode::ABatteryCollectorGameMode()
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+	DecayRate = 0.01f;
+}
+
+void ABatteryCollectorGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	// Si el jugador es una instancia de BatteryCollectorCharacter
+	// decrementamos el valor actual de energía
+	if (MyCharacter && MyCharacter->GetCurrentPower() > 0)
+	{
+		MyCharacter->UpdatePower(-DeltaTime * DecayRate * (MyCharacter->GetInitialPower()));
 	}
 }
--- EX002_Battery_Collection_Series/BatteryCollector/Source/BatteryCollector/BatteryCollectorGameMode.h
+++ EX002_Battery_Collection_Series/BatteryCollector/Source/BatteryCollector/BatteryCollectorGameMode.h
@@ -10,6 +10,12 @@ class ABatteryCollectorGameMode : public AGameMode
 
 public:
 	ABatteryCollectorGameMode();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	/** Velocidad a la que decae la energía del personaje */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power")
+	float DecayRate;
 };
 
 
